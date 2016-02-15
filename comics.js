bd0cf59b90e16b7dbd38c188d03ec00f64c54c83
Paulo Alexandre <paulequilibrio@gmail.com>
2016-02-15 18:41:47 -0300

    Better dimensions

--- comics.js
+++ comics.js
@@ -13,11 +13,12 @@ comics.get("/:comic", function(req, res) {
     res.send(fs.readFileSync("./public/index.html"))
 })
 
+
+
 /* xkcd */
 comics.get("/api/v1/xkcd", function(req, res) {
-    feed("http://xkcd.com/atom.xml", function(err, articles) {
-        if (err) throw err;
-        // console.log(JSON.stringify(articles[0]), null, 2);
+    feed("http://xkcd.com/atom.xml", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -25,9 +26,9 @@ comics.get("/api/v1/xkcd", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].link, '#comic img@title')(function(err, title) {
-            data.text = title
-            xray(articles[0].link, '#comic img@src')(function(err, comic) {
+        xray(articles[0].link, '#comic img@title')(function(error, text) {
+            data.text = text
+            xray(articles[0].link, '#comic img@src')(function(error, comic) {
                 data.comic = comic
                 res.send(data)
             })
@@ -35,19 +36,18 @@ comics.get("/api/v1/xkcd", function(req, res) {
     });
 });
 
+
 comics.get("/api/v1/xkcd/:id", function(req, res) {
-    // console.log(JSON.stringify(articles, null, 2));
-    // res.send('<pre>'+JSON.stringify(articles, null, 2)+'</pre>');
-    // xray(articles[0].link, '#comic img@src')(function(err, comic) {
-    xray('http://xkcd.com/'+req.params.id, '#comic img@src')(function(err, comic) {
+    xray('http://xkcd.com/'+req.params.id, '#comic img@src')(function(error, comic) {
         res.send(comic)
     })
 });
 
+
 /* Will Tirando */
 comics.get("/api/v1/willtirando", function(req, res) {
-    feed("http://www.willtirando.com.br/rss/", function(err, articles) {
-        if (err) throw err;
+    feed("http://www.willtirando.com.br/rss/", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -55,20 +55,18 @@ comics.get("/api/v1/willtirando", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].content, 'img@src')(function(err, comic) {
+        xray(articles[0].content, 'img@src')(function(error, comic) {
             data.comic = comic
-            // if ( articles[1].content.split('center;">')[1] ) {
-            //     data.text = articles[1].content.split('center;">')[1].slice(0,-5);
-            // }
             res.send(data)
         })
     });
 });
 
+
 /* Explosm */
 comics.get("/api/v1/explosm", function(req, res) {
-    feed("http://feeds.feedburner.com/Explosm", function(err, articles) {
-        if (err) throw err;
+    feed("http://feeds.feedburner.com/Explosm", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -76,17 +74,18 @@ comics.get("/api/v1/explosm", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].link, '#main-comic@src')(function(err, comic){
+        xray(articles[0].link, '#main-comic@src')(function(error, comic){
             data.comic = comic
             res.send(data)
         })
     });
 });
 
+
 /* Dilbert */
 comics.get("/api/v1/dilbert", function(req, res) {
-    feed("http://feed.dilbert.com/dilbert/daily_strip", function(err, articles) {
-        if (err) throw err;
+    feed("http://feed.dilbert.com/dilbert/daily_strip", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -94,9 +93,9 @@ comics.get("/api/v1/dilbert", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].link, 'span.comic-title-name')(function(err, title){
+        xray(articles[0].link, 'span.comic-title-name')(function(error, title){
             data.title = title;
-            xray(articles[0].link, '.img-comic@src')(function(err, comic){
+            xray(articles[0].link, '.img-comic@src')(function(error, comic){
                 data.comic = comic
                 res.send(data)
             })
@@ -104,10 +103,11 @@ comics.get("/api/v1/dilbert", function(req, res) {
     });
 });
 
+
 /* Dr. Pepper */
 comics.get("/api/v1/drpepper", function(req, res) {
-    feed("http://feeds.feedburner.com/drpepper", function(err, articles) {
-        if (err) throw err;
+    feed("http://feeds.feedburner.com/drpepper", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -115,9 +115,9 @@ comics.get("/api/v1/drpepper", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].content, 'p')(function(err, p){
-            data.text = p;
-            xray(articles[0].content, 'img@src')(function(err, comic){
+        xray(articles[0].content, 'p')(function(error, text){
+            data.text = text;
+            xray(articles[0].content, 'img@src')(function(error, comic){
                 data.comic = comic
                 res.send(data)
             })
@@ -128,8 +128,8 @@ comics.get("/api/v1/drpepper", function(req, res) {
 
 /* Hacktoon! */
 comics.get("/api/v1/hacktoon", function(req, res) {
-    feed("http://hacktoon.com/feed/rss.xml", function(err, articles) {
-        if (err) throw err;
+    feed("http://hacktoon.com/feed/rss.xml", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -137,17 +137,18 @@ comics.get("/api/v1/hacktoon", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].content, 'img@src')(function(err, comic){
+        xray(articles[0].content, 'img@src')(function(error, comic){
             data.comic = comic
             res.send(data)
         })
     });
 });
+
 
 /* Vida de Programador */
 comics.get("/api/v1/vidadeprogramador", function(req, res) {
-    feed("http://feeds.feedburner.com/VidaDeProgramador", function(err, articles) {
-        if (err) throw err;
+    feed("http://feeds.feedburner.com/VidaDeProgramador", function(error, articles) {
+        if (error) throw error;
         var data = {
             comic: '',
             author: articles[0].feed.name,
@@ -155,30 +156,36 @@ comics.get("/api/v1/vidadeprogramador", function(req, res) {
             source: articles[0].link,
             published: articles[0].published
         }
-        xray(articles[0].content, 'img@src')(function(err, comic){
-            data.comic = comic
-            res.send(data)
+        xray(articles[0].content, 'img@title')(function(error, text) {
+            data.text = text
+            xray(articles[0].content, 'img@src')(function(error, comic) {
+                data.comic = comic
+                res.send(data)
+            })
         })
     });
 });
 
 
-
 /* PHD Comics */
-// comics.get("/api/v1/phdcomics", function(req, res) {
-//     feed("http://phdcomics.com/gradfeed.php", function(err, articles) {
-//         if (err) throw err;
-//         console.log(articles)
-//         var data = {
-//             comic: '',
-//             author: articles[0].feed.name,
-//             title: articles[0].title,
-//             source: articles[0].link,
-//             published: articles[0].published
-//         }
-//         xray(articles[0].content, 'img@src')(function(err, comic){
-//             data.comic = comic
-//             res.send(data)
-//         })
-//     });
-// });
+comics.get("/api/v1/phdcomics", function(req, res) {
+    xray("http://www.feedbucket.com/?src=http://phdcomics.com/gradfeed.php", {
+        comic: 'img@src',
+        author: 'a[href="http://www.phdcomics.com"]',
+        title: 'font[size="-2"] a',
+        source: 'font[size="-2"] a@href',
+        published: 'font[size="1"] b',
+    })(function(error, data){
+        if (error) throw error;
+        res.send(data)
+    })
+});
+
+
+comics.get("/api/v1/:invalid", function(req, res) {
+    var data = {
+        author: '',
+        invalid: req.params.invalid,
+    };
+    res.send(data);
+});
