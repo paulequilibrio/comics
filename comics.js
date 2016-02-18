6f51c5ab8505c341c52973e292bd544f4824a03c
Paulo Alexandre <paulequilibrio@gmail.com>
2016-02-18 15:07:54 -0300

    Some improvements on code

--- comics.js
+++ comics.js
@@ -1,7 +1,7 @@
 var comics = require("./config.js")
 var fs     = require("fs")
 var feed   = require("feed-read")
-var xray   = require('x-ray')();
+var xray   = require('x-ray')()
 
 comics.get("/", function(req, res) {
     res.setHeader("Content-Type", "text/html")
@@ -15,159 +15,102 @@ comics.get("/:comic", function(req, res) {
 
 
 
-/* xkcd */
-comics.get("/api/v1/xkcd", function(req, res) {
-    feed("http://xkcd.com/atom.xml", function(error, articles) {
-        if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].link, '#comic img@title')(function(error, text) {
-            data.text = text
-            xray(articles[0].link, '#comic img@src')(function(error, comic) {
-                data.comic = comic
-                res.send(data)
-            })
-        })
-    });
-});
-
-
-comics.get("/api/v1/xkcd/:id", function(req, res) {
-    xray('http://xkcd.com/'+req.params.id, '#comic img@src')(function(error, comic) {
-        res.send(comic)
-    })
-});
-
-
-/* Will Tirando */
 comics.get("/api/v1/willtirando", function(req, res) {
     feed("http://www.willtirando.com.br/rss/", function(error, articles) {
         if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].content, 'img@src')(function(error, comic) {
-            data.comic = comic
+        xray(articles[0].content, {
+            comic: 'img@src'
+        })(function(error, data) {
+            data.author = articles[0].feed.name
+            data.title = articles[0].title
+            data.source = articles[0].link
+            data.published = articles[0].published
             res.send(data)
         })
-    });
-});
+    })
+})
 
 
-/* Explosm */
-comics.get("/api/v1/explosm", function(req, res) {
-    feed("http://feeds.feedburner.com/Explosm", function(error, articles) {
-        if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: '', //articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].link, '#main-comic@src')(function(error, comic){
-            data.comic = comic
-            res.send(data)
-        })
-    });
-});
-
-
-/* Dilbert */
-comics.get("/api/v1/dilbert", function(req, res) {
-    feed("http://feed.dilbert.com/dilbert/daily_strip", function(error, articles) {
-        if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: '',
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].link, 'span.comic-title-name')(function(error, title){
-            data.title = title;
-            xray(articles[0].link, '.img-comic@src')(function(error, comic){
-                data.comic = comic
-                res.send(data)
-            })
-        })
-    });
-});
-
-
-/* Dr. Pepper */
 comics.get("/api/v1/drpepper", function(req, res) {
     feed("http://feeds.feedburner.com/drpepper", function(error, articles) {
         if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].content, 'p')(function(error, text){
-            data.text = text;
-            xray(articles[0].content, 'img@src')(function(error, comic){
-                data.comic = comic
-                res.send(data)
-            })
-        })
-    });
-});
-
-
-/* Hacktoon! */
-comics.get("/api/v1/hacktoon", function(req, res) {
-    feed("http://hacktoon.com/feed/rss.xml", function(error, articles) {
-        if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].content, 'img@src')(function(error, comic){
-            data.comic = comic
+        xray(articles[0].content, {
+            text: 'p',
+            comic: 'img@src',
+        })(function(error, data){
+            if (error) throw error;
+            data.author = articles[0].feed.name
+            data.title = articles[0].title
+            data.source = articles[0].link
+            data.published = articles[0].published
             res.send(data)
         })
-    });
-});
+    })
+})
 
 
-/* Vida de Programador */
 comics.get("/api/v1/vidadeprogramador", function(req, res) {
     feed("http://feeds.feedburner.com/VidaDeProgramador", function(error, articles) {
         if (error) throw error;
-        var data = {
-            comic: '',
-            author: articles[0].feed.name,
-            title: articles[0].title,
-            source: articles[0].link,
-            published: articles[0].published
-        }
-        xray(articles[0].content, 'img@title')(function(error, text) {
-            data.text = text
-            xray(articles[0].content, 'img@src')(function(error, comic) {
-                data.comic = comic
-                res.send(data)
-            })
+        xray(articles[0].content, {
+            text: 'img@title',
+            comic: '.tirinha img@src',
+            video: 'a@href',
+        })(function(error, data) {
+            data.author = articles[0].feed.name
+            data.title = articles[0].title
+            data.source = articles[0].link
+            data.published = articles[0].published
+            data.text = (data.text) ? 'Camiseta: ' + data.text : undefined
+            data.video = (data.video.split('?v=')[1]) ? data.video : undefined
+            res.send(data)
         })
-    });
-});
+    })
+})
 
 
-/* PHD Comics */
+comics.get("/api/v1/xkcd", function(req, res) {
+    feed("http://xkcd.com/atom.xml", function(error, articles) {
+        if (error) throw error;
+        xray(articles[0].link, {
+            comic: '#comic img@src',
+            text: '#comic img@title',
+        })(function(error, data) {
+            if (error) throw error;
+            data.author = articles[0].feed.name
+            data.title = articles[0].title
+            data.source = articles[0].link
+            data.published = articles[0].published
+            res.send(data)
+        })
+    })
+})
+comics.get("/api/v1/xkcd/:id", function(req, res) {
+    xray('http://xkcd.com/'+req.params.id, '#comic img@src')
+    (function(error, comic) {
+        if (error) throw error;
+        res.send(comic)
+    })
+})
+
+
+comics.get("/api/v1/hacktoon", function(req, res) {
+    feed("http://hacktoon.com/feed/rss.xml", function(error, articles) {
+        if (error) throw error;
+        xray(articles[0].content, {
+            comic: 'img@src'
+        })(function(error, data){
+            if (error) throw error;
+            data.author = articles[0].feed.name
+            data.title = articles[0].title
+            data.source = articles[0].link
+            data.published = articles[0].published
+            res.send(data)
+        })
+    })
+})
+
+
 comics.get("/api/v1/phdcomics", function(req, res) {
     xray("http://www.feedbucket.com/?src=http://phdcomics.com/gradfeed.php", {
         comic: 'img@src',
@@ -179,13 +122,44 @@ comics.get("/api/v1/phdcomics", function(req, res) {
         if (error) throw error;
         res.send(data)
     })
-});
+})
+
+
+comics.get("/api/v1/explosm", function(req, res) {
+    feed("http://feeds.feedburner.com/Explosm", function(error, articles) {
+        if (error) throw error;
+        xray(articles[0].link, {
+            comic: '#main-comic@src',
+            video: '#comic-container a@href',
+        })(function(error, data){
+            if (error) throw error;
+            data.author = articles[0].feed.name
+            data.published = articles[0].published
+            data.source = (!data.video) ? articles[0].link : data.video
+            data.video = undefined
+            res.send(data)
+        })
+    })
+})
+
+
+comics.get("/api/v1/dilbert", function(req, res) {
+    feed("http://feed.dilbert.com/dilbert/daily_strip", function(error, articles) {
+        if (error) throw error;
+        xray(articles[0].link, {
+            title: 'span.comic-title-name',
+            comic: '.img-comic@src',
+        })(function(error, data){
+            data.author = articles[0].feed.name
+            data.source = articles[0].link
+            data.published = articles[0].published
+            res.send(data)
+        })
+    })
+})
 
 
 comics.get("/api/v1/:invalid", function(req, res) {
-    var data = {
-        author: '',
-        invalid: req.params.invalid,
-    };
+    var data = { invalid: req.params.invalid }
     res.send(data);
 });
